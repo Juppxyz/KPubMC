451b992992b76300cffa388abfc9452306a04560
Danny Koehler <54675079+vyltra@users.noreply.github.com>
2024-11-21 20:40:44 +0100

    Revert "WIP"
    This reverts commit 5e175c1ddf917c04e5bbafc9024b2675e373bf53.

--- src/main/java/xyz/jupp/minecraft/Main.java
+++ src/main/java/xyz/jupp/minecraft/Main.java
@@ -37,7 +37,6 @@ public final class Main extends JavaPlugin {
         this.getCommand("team").setExecutor(new TeamCommand());
         this.getCommand("ranking").setExecutor(new RankingCommand());
         this.getCommand("createshop").setExecutor(new CreateShopCommand());
-        this.getCommand("createblackjack").setExecutor(new CreateBlackJackCommand());
         this.getCommand("hover").setExecutor(new HoverTextCommand());
         this.getCommand("kopf").setExecutor(new PlayerHeadsCommand());
         this.getCommand("head").setExecutor(new PlayerHeadsCommand());
@@ -55,7 +54,6 @@ public final class Main extends JavaPlugin {
         Bukkit.getPluginManager().registerEvents(new NetherTransferListener(), this);
         Bukkit.getPluginManager().registerEvents(new DeathListener(), this);
         Bukkit.getPluginManager().registerEvents(new ShopListener(), this);
-        Bukkit.getPluginManager().registerEvents(new BlackJackListener(), this);
         Bukkit.getPluginManager().registerEvents(new CreateLocalShopListener(), this);
         Bukkit.getPluginManager().registerEvents(new WarpInventoryListener(), this);
 
